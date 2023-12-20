8ec2bc0515c16db23881916f204a7e3077f43a1c
Superhero <wael22ka@hotmail.com>
2023-12-20 23:36:27 +0100

    This is near to final

--- application/forms.py
+++ application/forms.py
@@ -1,10 +1,35 @@
+from flask_login import UserMixin
 from flask_wtf import FlaskForm
-from wtforms import StringField, EmailField, SubmitField, PasswordField
+from wtforms import StringField, EmailField, SubmitField, PasswordField, TextAreaField
 from wtforms.validators import DataRequired
 
 class UserInfo(FlaskForm):
-    username = StringField("Username", validators= [DataRequired()])
-    email = EmailField("E-mail", validators= [DataRequired()])
-    password = PasswordField("Password", validators= [DataRequired()])
+    username = StringField("Username", validators=[DataRequired()])
+    email = EmailField("E-mail", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirmPassword = PasswordField("Password (Confirm)", validators=[DataRequired()])
     submit = SubmitField("Sign Up")
+
+class User(UserMixin):
+    def __init__(self, user_data):
+        self.id = str(user_data['_id'])
+        self.username = user_data['username']
+        self.email = user_data['email']
+        self.password = user_data['password']
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.id
+class Note():
+    data = TextAreaField("data", validators= [DataRequired()])
+
+
 
--- application/routes.py
+++ application/routes.py
@@ -1,34 +1,81 @@
-from application import app
-from flask import request, redirect, render_template, url_for, flash
-
-from .forms import UserInfo
-from datetime import datetime
-from application import db
-
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
 
 from application import app
-from flask import request, redirect, render_template, flash
-from .forms import UserInfo
+from flask import request, redirect, render_template, flash, url_for
+from .forms import UserInfo, Note, User
 from datetime import datetime
 from application import db
 
 @app.route("/", methods=['POST', 'GET'])
-def index():
+def signup():
     form = UserInfo()
 
     if form.validate_on_submit():
         username = form.username.data
         email = form.email.data
+        password= form.password.data
+        confirmPassword= form.confirmPassword.data
+
+        user = db.user_data.find_one({'email': email})
+
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(username) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password != confirmPassword:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            hashed_password = password
+            new_user = {
+                'email': email,
+                'first_name': username,
+                'password': hashed_password,
+                'date_created': datetime.utcnow()
+            }
+            db.user_data.insert_one(new_user)
+            flash('Account created!', category='success')
+            return redirect(url_for('login'))
+    return render_template("signUp.html", title="Sign Up", form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = UserInfo()
+    if request.method == 'POST' and form.validate_on_submit():
+        email = form.email.data
         password = form.password.data
 
-        db.user_data.insert_one({
-            "username": username,
-            "email": email,
-            "password": password,
-            "date_created": datetime.utcnow()
-        })
+        user_data = db.user_data.find_one({'email': email})
 
-        flash("Signed up successfully", "success")
-        return redirect("/")
+        if user_data and user_data['password'] == password:
+            user = User(user_data)
+            login_user(user, remember=True)
+            flash('Logged in successfully!', category='success')
+            return redirect(url_for('home'))
+        else:
+            flash('Incorrect email or password, try again.', category='error')
 
-    return render_template("SignUp.html", title="Sign Up", form=form)
+    return render_template("logIn.html", user=current_user, form=form)
+@app.route('/', methods=['GET', 'POST'])
+@login_required
+def home():
+    if request.method == 'POST':
+        note_data = Note.data.data  # Assuming your HTML form has an input field with name 'note'
+
+        if len(note_data) < 1:
+            flash('Note is too short!', category='error')
+        else:
+            new_note = {
+                'note': note_data,
+                'date_created': datetime.utcnow(),
+                'id_user_created': current_user._id
+            }
+            db.user_data.insert_one(new_note)
+
+            flash('Note added!', category='success')
+
+    return render_template("home.html", user=current_user)
